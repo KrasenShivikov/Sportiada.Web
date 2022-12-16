a861792ffe6424b31f76cb59987c81e9fca935d4
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:36:24 +0200

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,5 @@
 ﻿namespace Sportiada.Web
 {
-    using AutoMapper;
     using Data.Models;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Identity;
@@ -13,6 +12,8 @@
     using Services.AlpineSki.Implementations;
     using Services.Football.Interfaces;
     using Services.Football.Implementations;
+    using Services.Admin.Interfaces;
+    using Services.Admin.Implementations;
 
     public class Startup
     {
@@ -35,15 +36,52 @@
 
             services.AddMvc();
 
-            services.AddAutoMapper();
-
             services.AddTransient<ICompetitionAlpineSkiService, CompetitionAlpineSkiService>();
             services.AddTransient<ISkierAlpineSkiService, SkierAlpineSkiService>();
             services.AddTransient<IResultAlpineSkiService, ResultAlpineSkiService>();
-            services.AddTransient<ISideService, SideService>();
             services.AddTransient<IGameService, GameService>();
             services.AddTransient<IPlayerService, PlayerService>();
             services.AddTransient<ITeamService, TeamService>();
+            services.AddTransient<ISeasonService, SeasonService>();
+            services.AddTransient<IRoundService, RoundService>();
+            services.AddTransient<ICountryAdminService, CountryAdminService>();
+            services.AddTransient<IFootballPlayerAdminService, FootballPlayerAdminService>();
+            services.AddTransient<ICityAdminService, CityAdminService>();
+            services.AddTransient<IFootballTeamAdminService, FootballTeamAdminService>();
+            services.AddTransient<ISeasonAdminService, SeasonAdminService>();
+            services.AddTransient<IFootballTournamentService, FootballTournamentService>();
+            services.AddTransient<IFootballCompetitionAdminService, FootballCompetitionAdminService>();
+            services.AddTransient<ICompetitionTypeAdminService, CompetitionTypeAdminService>();
+            services.AddTransient<IFootballRoundAdminService, FootballRoundAdminService>();
+            services.AddTransient<IFootballCardTypeAdminService, FootballCardTypeAdminService>();
+            services.AddTransient<IFootballGoalTypeAdminService, FootballGoalTypeAdminService>();
+            services.AddTransient<IFootballSidelineReasonAdminService, FootballSidelineReasonAdminService>();
+            services.AddTransient<IFootballRefereeTypeAdminService, FootballRefereeTypeAdminService>();
+            services.AddTransient<IFootballRefereeAdminService, FootballRefereeAdminService>();
+            services.AddTransient<IFootballPlayerTransferAdminService, FootballPlayerTransferAdminService>();
+            services.AddTransient<IFootballPlayerCountryAdminService, FootballPlayerCountryAdminService>();
+            services.AddTransient<IFootballSquadAdminService, FootballSquadAdminService>();
+            services.AddTransient<IFootballSquadPlayerAdminService, FootballSquadPlayerAdminService>();
+            services.AddTransient<IFootballGameAdminService, FootballGameAdminService>();
+            services.AddTransient<IFootballCoachAdminService, FootballCoachAdminService>();
+            services.AddTransient<IFootballCoachTeamAdminService, FootballCoachTeamAdminService>();
+            services.AddTransient<IFootballSquadCoachAdminService, FootballSquadCoachAdminService>();
+            services.AddTransient<IFootballGameStatisticAdminService, FootballGameStatisticAdminService>();
+            services.AddTransient<IFootballLineUpAdminService, FootballLineUpAdminService>();
+            services.AddTransient<IFootballReserveAdminService, FootballReserveAdminService>();
+            services.AddTransient<IFootballGoalAdminService, FootballGoalAdminService>();
+            services.AddTransient<IFootballGoalTypeAdminService, FootballGoalTypeAdminService>();
+            services.AddTransient<IFootballGoalAssistanceAdminService, FootballGoalAssistanceAdminService>();
+            services.AddTransient<IFootballCardAdminService, FootballCardAdminService>();
+            services.AddTransient<IFootballSidelineAdminService, FootballSidelineAdminService>();
+            services.AddTransient<IFootballSubstituteAdminService, FootballSubstituteAdminService>();
+            services.AddTransient<IFootballPlayerInAdminService, FootballPlayerInAdminService>();
+            services.AddTransient<IFootballPlayerOutAdminService, FootballPlayerOutAdminService>();
+            services.AddTransient<IFootballGameRefereeAdminService, FootballGameRefereeAdminService>();
+            services.AddTransient<IFootballGameStatisticCoachAdminService, FootballGameStatisticCoachAdminService>();
+            services.AddTransient<IFootballStadiumAdminService, FootballStadiumAdminService>();
+            services.AddTransient<ISquadService, SquadService>();
+            services.AddTransient<ICoachService, CoachService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
