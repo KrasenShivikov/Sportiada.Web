e892015270eb2f84228cb648766ff8fd739202ce
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:37:59 +0200

    Add files via upload

---
+++ wwwroot/js/InsertIcon.js
@@ -0,0 +1,15 @@
+﻿const iconPath = '~/images/FootballGameIcons/';
+const sliceIndex = 12;
+
+let i = document.getElementById('i');
+let i_trigger = document.getElementById('i_trigger');
+let i_file = document.getElementById('i_file');
+handleEventListener(i_trigger, i, i_file, sliceIndex, iconPath);
+
+function handleEventListener(trigger, element, fileElement, index, path) {
+    trigger.addEventListener('click', () => {
+        let fileElement_Text = fileElement.value.slice(index);
+        let fullPath = path + fileElement_Text;
+        element.value = fullPath;
+    })
+}
---
+++ wwwroot/js/Search.js
@@ -0,0 +1,24 @@
+﻿const btn = document.getElementById('btn');
+const tbody = document.getElementById('tbody');
+const elements = Array.from(tbody.children);
+
+btn.addEventListener('click', () => {
+    const search = document.getElementById('search');
+    const tbody = document.getElementById('tbody');
+    const arr = Array.from(tbody.children);
+
+    const value = search.value;
+    const results = elements.filter(t => t.children[0].textContent.toLowerCase().includes(value.toLowerCase()));
+
+    if (value != '') {
+        arr.forEach(t => {
+            tbody.removeChild(t);
+        })
+
+        results.forEach(r => {
+            tbody.appendChild(r);
+        })
+    }
+
+
+})
