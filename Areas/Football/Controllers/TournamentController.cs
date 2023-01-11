8a0c6c57ea5397f6339774374535fd01258b0d48
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2023-01-11 15:48:29 +0200

    Add files via upload

---
+++ Areas/Football/Controllers/TournamentController.cs
@@ -0,0 +1,31 @@
+﻿namespace Sportiada.Web.Areas.Football.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Football.Interfaces;
+    using Services.Football.Models.Tournament;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Area("Football")]
+    public class TournamentController : Controller
+    {
+        private readonly IGameService games;
+        public TournamentController(IGameService games)
+        {
+            this.games = games;
+        }
+
+        [Route("football/tournament/{tournamentId}/{seasonId}")]
+        public IActionResult OverallStatisticByIdBySeasonId(int tournamentId, int seasonId)
+        {
+            var model = new TournamentSeasonStatisticModel
+            {
+                Games = games.GamesByTournamentBySeason(tournamentId, seasonId)
+            };
+
+            return View(model);
+        }
+    }
+}
